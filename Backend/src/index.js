cb1fb9ffa8004147e9e50f79ee0559ca8b3c9569
Abhishek Kumar Pundir <abhishekgigaiw@gmail.com>
2025-12-07 12:57:39 +0530

    cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads",cors(), express.static("uploads"));
 app.use("/api/playlist", playlistRoutes);
 app.use("/api/stats", statsRoutes);
 app.use('/api/auth', authRoutes);
