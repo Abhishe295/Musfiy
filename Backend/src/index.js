3d1fe15bc6051bb6cfafd654a2b8adbad685c639
Abhishek Kumar Pundir <abhishekgigaiw@gmail.com>
2025-12-07 13:15:18 +0530

    songs upload

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,18 +10,17 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+
+
+
+
 dotenv.config();
 const PORT = process.env.PORT || 6001;
-const app = express();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL
-];
-
+const app  = express();
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL, ]
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
+    if (!origin) return callback(null, true); // mobile / postman
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
@@ -30,35 +29,14 @@ app.use(cors({
   credentials: true,
 }));
 
-// CORS for audio streaming
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const isRender = process.env.RENDER === "true";
-
-let uploadsPath = "";
-
-if (isRender) {
-  uploadsPath = "/opt/render/project/src/uploads";
-} else {
-  uploadsPath = path.join(__dirname, "..", "uploads");
-}
-
-console.log("SERVING UPLOADS FROM:", uploadsPath);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// FIX HERE: remove extra cors()
-app.use("/uploads", express.static(uploadsPath));
 
+app.use("/uploads",cors(), express.static("uploads"));
 app.use("/api/playlist", playlistRoutes);
 app.use("/api/stats", statsRoutes);
 app.use('/api/auth', authRoutes);
